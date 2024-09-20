d1d4128a26d70923b1b693cd50b164e6fd7805b0
Alexandre Gros <xelaflash@gmail.com>
2024-09-20 10:14:44 +0200

    chore: add comment for ReactDom.render deprecation warning

--- src/index.tsx
+++ src/index.tsx
@@ -4,20 +4,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import App from './App';
-import createStore from './redux/store'
+import createStore from './redux/store';
 import { receiveDomains } from './redux/domains/actions';
 
 const store = createStore();
 
-store.dispatch(receiveDomains([
-  'US_OK-WOK',
-  'FR_NK-WOL',
-  'FR_OK-NPP',
-  'EN_NK-NRP',
-  'EN_BL-WOL',
-]))
+store.dispatch(receiveDomains(['US_OK-WOK', 'FR_NK-WOL', 'FR_OK-NPP', 'EN_NK-NRP', 'EN_BL-WOL']));
 
 ReactDOM.render(
-  <Provider store={store}><App /></Provider>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
 );
+
+// ReatDom.render is deprecated, hence the warning in the console
+// We should use createRoot instead
+// const container = document.getElementById('app');
+// const root = createRoot(container!);
+// Or inline
+// const root = ReactDOM.createRoot(document.getElementById('root')!);
+// root.render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+// );
+
+// But file is marked as forbidden to modify!
